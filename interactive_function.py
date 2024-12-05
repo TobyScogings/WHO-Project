07b4873d5bd32da03e58a18a889f05597df38701
Vivien <viviensiu@gmail.com>
2024-12-05 16:48:28 +0000

    remove EDA packages from streamlit script

--- interactive_function.py
+++ interactive_function.py
@@ -3,8 +3,8 @@
 
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+# import seaborn as sns
+# import matplotlib.pyplot as plt
 
 # from sklearn.model_selection import train_test_split
 
